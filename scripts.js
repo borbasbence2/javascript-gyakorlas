825cfa93b0bfefa9cff4521e9f12542c350f225c
borbasbence2 <63309517+borbasbence2@users.noreply.github.com>
2020-04-10 15:21:27 +0200

    stream

---
+++ scripts.js
@@ -0,0 +1,40 @@
+console.log("JavaScript is working!");
+
+let colors = ["orange", "blue", "green", "purple"];
+
+let post = {
+    title: "My Post",
+    description: "My First post, awesome",
+    likeCount: 12,
+    comments: ["cool!","nice job!"]
+  };
+  
+
+  console.log(colors);
+  console.log(post);
+
+  let age = 12;
+
+  if (age > 18) {
+      console.log("you're an adult!")
+  } else{
+      console.log("you're a kid!")
+  }
+ 
+  for (let i = 0; i < 10; i++) {
+      console.log(i);
+      
+  }  
+
+  console.log(colors);
+  
+  let myFunction = x => {
+      console.log(x)
+  }
+
+  colors.forEach(colors => {
+      console.log(colors)
+  } )
+  
+  
+  console.log("this is the end of our code")
